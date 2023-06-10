0799ed16221acc90096fbd1ce9fac9466470776d
VMark-buribubi <plants.mark@gmail.com>
2023-06-10 20:46:17 +0200

    add canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { LogoutComponent } from './logout/logout.component';
 import { UpdateStudentComponent } from './update-student/update-student.component';
 import { UpdateTeacherComponent } from './update-teacher/update-teacher.component';
 import { UpdateSubjectComponent } from './update-subject/update-subject.component';
+import { ApiService } from './api.service';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -19,13 +20,13 @@ const routes: Routes = [
   { path: 'list-teachers', component: ListTeachersComponent },
   { path: 'list-subjects', component: ListTeachersComponent },
 
-  { path: 'create-student', component: ListTeachersComponent canActivate: [ApiService]},
-  { path: 'create-subject', component: ListTeachersComponent canActivate: [ApiService]},
-  { path: 'create-teacher', component: ListTeachersComponent canActivate: [ApiService]},
+  { path: 'create-student', component: ListTeachersComponent, canActivate: [ApiService]},
+  { path: 'create-subject', component: ListTeachersComponent, canActivate: [ApiService]},
+  { path: 'create-teacher', component: ListTeachersComponent, canActivate: [ApiService]},
 
-  { path: 'update-student', component: UpdateStudentComponent canActivate: [ApiService]},
-  { path: 'update-teacher', component: UpdateTeacherComponent canActivate: [ApiService]},
-  { path: 'update-subject', component: UpdateSubjectComponent canActivate: [ApiService]},
+  { path: 'update-student', component: UpdateStudentComponent, canActivate: [ApiService]},
+  { path: 'update-teacher', component: UpdateTeacherComponent, canActivate: [ApiService]},
+  { path: 'update-subject', component: UpdateSubjectComponent, canActivate: [ApiService]},
 
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
