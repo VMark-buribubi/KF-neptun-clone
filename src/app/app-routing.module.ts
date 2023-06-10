893627a1d55e342ae8cfca5f68ce674e09b20665
VMark-buribubi <plants.mark@gmail.com>
2023-06-10 12:30:49 +0200

    add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ListStudentsComponent } from './list-students/list-students.component';
+import { ListTeachersComponent } from './list-teachers/list-teachers.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'list-students', component: ListStudentsComponent },
+  { path: 'list-teachers', component: ListTeachersComponent },
+  { path: 'list-subjects', component: ListTeachersComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
