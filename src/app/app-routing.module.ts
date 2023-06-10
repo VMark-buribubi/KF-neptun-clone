14c5295e788fdbb76dddc82700fb945d21312d8d
VMark-buribubi <plants.mark@gmail.com>
2023-06-10 20:43:30 +0200

    add route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { RegisterComponent } from './register/register.component';
 import { ListStudentsComponent } from './list-students/list-students.component';
 import { ListTeachersComponent } from './list-teachers/list-teachers.component';
 import { LogoutComponent } from './logout/logout.component';
+import { UpdateStudentComponent } from './update-student/update-student.component';
+import { UpdateTeacherComponent } from './update-teacher/update-teacher.component';
+import { UpdateSubjectComponent } from './update-subject/update-subject.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -15,6 +18,15 @@ const routes: Routes = [
   { path: 'list-students', component: ListStudentsComponent },
   { path: 'list-teachers', component: ListTeachersComponent },
   { path: 'list-subjects', component: ListTeachersComponent },
+
+  { path: 'create-student', component: ListTeachersComponent canActivate: [ApiService]},
+  { path: 'create-subject', component: ListTeachersComponent canActivate: [ApiService]},
+  { path: 'create-teacher', component: ListTeachersComponent canActivate: [ApiService]},
+
+  { path: 'update-student', component: UpdateStudentComponent canActivate: [ApiService]},
+  { path: 'update-teacher', component: UpdateTeacherComponent canActivate: [ApiService]},
+  { path: 'update-subject', component: UpdateSubjectComponent canActivate: [ApiService]},
+
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
