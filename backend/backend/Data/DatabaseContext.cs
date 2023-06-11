c1440237f389fa4c21e9150c0cdfa2aef56a8d57
VMark-buribubi <plants.mark@gmail.com>
2023-06-11 23:37:16 +0200

    add testdata and change controllers

--- backend/backend/Data/DatabaseContext.cs
+++ backend/backend/Data/DatabaseContext.cs
@@ -22,6 +22,24 @@ namespace backend.Data
             modelBuilder.Entity<Teacher>()
                 .HasMany(s => s.Subjects)
                 .WithMany(p => p.Teachers);
+
+            // Add test data for Students
+            modelBuilder.Entity<Student>().HasData(
+                new Student { Id = Guid.NewGuid(), Name = "John Doe", Neptun = "ABC123", Image = "https://xsgames.co/randomusers/assets/avatars/pixel/0.jpg" },
+                new Student { Id = Guid.NewGuid(), Name = "Jane Smith", Neptun = "DEF456", Image = "https://xsgames.co/randomusers/assets/avatars/pixel/1.jpg" }
+            );
+
+            // Add test data for Subjects
+            modelBuilder.Entity<Subject>().HasData(
+                new Subject { Id = Guid.NewGuid(), Name = "Math", Neptun = "MATH101", Credit = 4, Exam = true, Image = "https://fastly.picsum.photos/id/229/200/300.jpg?hmac=WD1_MXzGKrVpaJj2Utxv7FoijRJ6h4S4zrBj7wmsx1U" },
+                new Subject { Id = Guid.NewGuid(), Name = "Science", Neptun = "SCI202", Credit = 3, Exam = false, Image = "https://fastly.picsum.photos/id/604/200/300.jpg?hmac=6ceMKS8u7easDoKzWSaIiSTpRlTPn1OUOdfSJWou3uQ" }
+            );
+
+            // Add test data for Teachers
+            modelBuilder.Entity<Teacher>().HasData(
+                new Teacher { Id = Guid.NewGuid(), Name = "Professor X", Neptun = "PROF01", Image = "https://xsgames.co/randomusers/assets/avatars/pixel/2.jpg" },
+                new Teacher { Id = Guid.NewGuid(), Name = "Dr. Watson", Neptun = "DRWAT02", Image = "https://xsgames.co/randomusers/assets/avatars/pixel/3.jpg" }
+            );
         }
     }
 }
