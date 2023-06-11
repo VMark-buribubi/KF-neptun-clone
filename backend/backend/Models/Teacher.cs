82c1dc3882717165d2453888b8c9d5056f6c1c03
VMark-buribubi <plants.mark@gmail.com>
2023-06-11 15:40:02 +0200

    models

---
+++ backend/backend/Models/Teacher.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models
+{
+    public class Teacher
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Neptun { get; set; }
+        public string Image { get; set; }
+        public string CreatorName { get; set; }
+    }
+}
